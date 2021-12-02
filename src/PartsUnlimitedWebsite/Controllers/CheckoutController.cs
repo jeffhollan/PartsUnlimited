45843d239854e76a4f3830ed7113c8e0b872f87b
Jeff Hollan <jeffhollan@outlook.com>
2021-12-02 12:58:50 -0800

    post demo

--- src/PartsUnlimitedWebsite/Controllers/CheckoutController.cs
+++ src/PartsUnlimitedWebsite/Controllers/CheckoutController.cs
@@ -56,29 +56,7 @@ namespace PartsUnlimited.Controllers
 
             try
             {
-                if (string.Equals(formCollection["PromoCode"].FirstOrDefault(), PromoCode,
-                    StringComparison.OrdinalIgnoreCase) == false)
-                {
-                    return View(order);
-                }
-                else
-                {
-                    order.Username = HttpContext.User.Identity.Name;
-                    order.OrderDate = DateTime.Now;
-
-                    //Add the Order
-                    _db.Orders.Add(order);
-
-                    //Process the order
-                    var cart = ShoppingCart.GetCart(_db, HttpContext);
-                    cart.CreateOrder(order);
-
-                    // Save all changes
-                    await _db.SaveChangesAsync(HttpContext.RequestAborted);
-
-                    return RedirectToAction("Complete",
-                        new { id = order.OrderId });
-                }
+                await client.PostAsJsonAsync(paymentController, formCollection);
             }
             catch
             {
